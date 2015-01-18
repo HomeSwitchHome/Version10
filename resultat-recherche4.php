ad85f74389f0f54c70f17b73e78f24684e36b283
Danny Canaan <danny.canaan@gmail.com>
2015-01-18 22:38:13 +0100

    page recherche aucun resultat

---
+++ resultat-recherche4.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Home Switch Home</title>
+		<link href="style.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div id="wrapper">
+				
+			<?php include("header.php") ?>
+		<div id="user_list">
+			<h3>Votre recherche n'a retourné aucun résultat</h3>
+			<h2>Voulez-vous : <a href="recherche.php">Effectuer une nouvelle recherche</a> ou <a href="index.php">Retourner à la page d'accueil ?</a></h2>
+		</div>
+			
+		</div>
+		<?php include("footer.php") ?>
+	</body>
+</html>
